f25e6d4eee9a53ad35f65ec8f947398fa3fb64d1
Diamond Welsh <diamond.welsh@gmail.com>
2020-12-13 17:05:21 -0800

    Updates

---
+++ precache-manifest.f4a0b3929876417ede04d86c8f436007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f40e69dd6c5f732a071726b0a6f4bc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "50ea58c725c78fd9811a",
+    "url": "/monsters-rolodex/static/css/main.eedaeb6e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ea58c725c78fd9811a",
+    "url": "/monsters-rolodex/static/js/main.e5dd2b8a.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
